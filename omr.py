f0650c71acda1c32342dec01056e706bb6c35986
Shebin Jose Jacob <shebinjosejacob@gmail.com>
2022-06-30 12:12:46 +0530

    Update omr.py

--- omr.py
+++ omr.py
@@ -11,7 +11,7 @@ runner = None
 # if you don't want to see a camera preview, set this to False
 show_camera = True
 if (sys.platform == 'linux' and not os.environ.get('DISPLAY')):
-    show_camera = False
+    show_camera = True
 
 def now():
     return round(time.time() * 1000)
@@ -19,7 +19,7 @@ def now():
 def get_webcams():
     port_ids = []
     for port in range(5):
-        print("Looking for a camera in port %s:" %port)
+        #print("Looking for a camera in port %s:" %port)
         camera = cv2.VideoCapture(port)
         if camera.isOpened():
             ret = camera.read()[0]
@@ -27,7 +27,7 @@ def get_webcams():
                 backendName =camera.getBackendName()
                 w = camera.get(3)
                 h = camera.get(4)
-                print("Camera %s (%s x %s) found in port %s " %(backendName,h,w, port))
+                #print("Camera %s (%s x %s) found in port %s " %(backendName,h,w, port))
                 port_ids.append(port)
             camera.release()
     return port_ids
@@ -64,13 +64,13 @@ def main(argv):
     dir_path = os.path.dirname(os.path.realpath(__file__))
     modelfile = os.path.join(dir_path, model)
 
-    #print('MODEL: ' + modelfile)
+    os.system('clear')
     answer_key=[]
+    
     while True:
         if (answer_key==[]):
             with ImageImpulseRunner(modelfile) as runner:
                     model_info = runner.init()
-                    #print('Loaded runner for "' + model_info['project']['owner'] + ' / ' + model_info['project']['name'] + '"')
                     labels = model_info['model_parameters']['labels']
                     if len(args)>= 2:
                         videoCaptureDeviceId = int(args[1])
@@ -79,7 +79,8 @@ def main(argv):
                         if len(port_ids) == 0:
                             raise Exception('Cannot find any webcams')
                         if len(args)<= 1 and len(port_ids)> 1:
-                            raise Exception("Multiple cameras found. Add the camera port ID as a second argument to use to this script")
+                            #raise Exception("Multiple cameras found. Add the camera port ID as a second argument to use to this script")
+                            print()
                         videoCaptureDeviceId = int(port_ids[0])
 
                     camera = cv2.VideoCapture(videoCaptureDeviceId)
@@ -88,7 +89,6 @@ def main(argv):
                         backendName = camera.getBackendName()
                         w = camera.get(3)
                         h = camera.get(4)
-                        #print("Camera %s (%s x %s) in port %s selected." %(backendName,h,w, videoCaptureDeviceId))
                         camera.release()
                     else:
                         raise Exception("Couldn't initialize selected camera.")
@@ -99,12 +99,12 @@ def main(argv):
                         if (next_frame > now()):
                             time.sleep((next_frame - now()) / 1000)
 
-                        y = input("Place your answerkey in position and press 'Y'")
+                        y = input("Place your answerkey in position and press 'Y' : ")
                         if (y=="Y"):
                             if "bounding_boxes" in res["result"].keys():
-                                print('Found %d bounding boxes (%d ms.)' % (len(res["result"]["bounding_boxes"]), res['timing']['dsp'] + res['timing']['classification']))
+                                #print('Found %d bounding boxes (%d ms.)' % (len(res["result"]["bounding_boxes"]), res['timing']['dsp'] + res['timing']['classification']))
                                 for bb in res["result"]["bounding_boxes"]:
-                                    print('\t%s (%.2f): x=%d y=%d w=%d h=%d' % (bb['label'], bb['value'], bb['x'], bb['y'], bb['width'], bb['height']))
+                                    #print('\t%s (%.2f): x=%d y=%d w=%d h=%d' % (bb['label'], bb['value'], bb['x'], bb['y'], bb['width'], bb['height']))
                                     img = cv2.rectangle(img, (bb['x'], bb['y']), (bb['x'] + bb['width'], bb['y'] + bb['height']), (255, 0, 0), 1)
                                     answer_key.append([bb['x'],bb['y']])
                                 print(answer_key)
@@ -120,7 +120,6 @@ def main(argv):
                 y = input("Place your answersheet in position and press 'Y'")
                 if (y=="Y"):
                     model_info = runner.init()
-                    #print('Loaded runner for "' + model_info['project']['owner'] + ' / ' + model_info['project']['name'] + '"')
                     labels = model_info['model_parameters']['labels']
                     if len(args)>= 2:
                         videoCaptureDeviceId = int(args[1])
@@ -129,7 +128,8 @@ def main(argv):
                         if len(port_ids) == 0:
                             raise Exception('Cannot find any webcams')
                         if len(args)<= 1 and len(port_ids)> 1:
-                            raise Exception("Multiple cameras found. Add the camera port ID as a second argument to use to this script")
+                            #raise Exception("Multiple cameras found. Add the camera port ID as a second argument to use to this script")
+                            print()
                         videoCaptureDeviceId = int(port_ids[0])
 
                     camera = cv2.VideoCapture(videoCaptureDeviceId)
@@ -138,7 +138,6 @@ def main(argv):
                         backendName = camera.getBackendName()
                         w = camera.get(3)
                         h = camera.get(4)
-                        #print("Camera %s (%s x %s) in port %s selected." %(backendName,h,w, videoCaptureDeviceId))
                         camera.release()
                     else:
                         raise Exception("Couldn't initialize selected camera.")
